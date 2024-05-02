60e85a8f21b994a3d25b9638efd78d938690e665
Edward Minaya <ed.miya.14@gmail.com>
2024-05-01 21:36:03 -0400

    header added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "purple-segundary": "#7F5AF0",
+        "black-primary": "#1E1E1E",
+        "gray-primary": "#94A1B2",
+      },
+    },
+  },
+  plugins: [],
+};
